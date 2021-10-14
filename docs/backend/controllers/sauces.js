77ae938ce87e179af738fcae158b4a6305386cc7
Ben <benoit.magnan@gmail.com>
2021-10-14 11:15:05 +0200

    Ajout de la fonction LIKE

--- docs/backend/controllers/sauces.js
+++ docs/backend/controllers/sauces.js
@@ -59,56 +59,50 @@ exports.getSauceById = (req, res, next) => {
 };
 
 exports.likeSauce = (req, res, next) => {
-    if (req.body.like == 0) {
-        ModelsSauce.findOne({_id: req.params.id})
-        .then((sauce) => {
-            if (sauce.usersLiked.includes(req.body.userId)){
-                ModelsSauce.updateOne(
-                    {_id: req.params.id}, 
-                    {$pull: {usersLiked: req.body.userId}},
-                    {likes: 0}
-                )
-                .then(() => res.status(200).json({message: 'Cette sauce ne vous intéresse plus !'}))
-                .catch(error => res.status(400).json({error}));  
-                console.log(sauce.usersLiked);
-                console.log(sauce.usersDisliked); 
-            }
-
-            if (sauce.usersDisliked.includes(req.body.userId)){
-                ModelsSauce.updateOne(
-                    {_id: req.params.id}, 
-                    {$pull: {usersDisliked: req.body.userId}},
-                    {dislikes: 0}
-                )
-                .then(() => res.status(200).json({message: 'Cette sauce ne vous intéresse plus !'}))
-                .catch(error => res.status(400).json({error}));   
-                console.log(sauce.usersLiked);
-                console.log(sauce.usersDisliked); 
-            }
-        })
-        .catch(error => res.status(400).json({error}));          
-    }
-
     if (req.body.like == 1) {
         ModelsSauce.updateOne(
             {_id: req.params.id}, 
-            {$push: {usersLiked: req.body.userId}},
-            {likes: 1}
+            {$push: {usersLiked: req.body.userId},
+            $inc: {likes: +1}}
         )
         .then(() => res.status(200).json({message: 'Sauce liké !'}))
         .catch(error => res.status(400).json({error})); 
     }
 
+    if (req.body.like == 0) {
+        ModelsSauce.findOne({_id: req.params.id})
+        .then((sauce) => {
+            if (sauce.usersLiked.includes(req.body.userId)){
+                ModelsSauce.updateOne(
+                    {_id: req.params.id},
+                    {$pull: {usersLiked: req.body.userId},
+                    $inc: {likes: -1}}
+                )
+                .then(() => res.status(200).json({message: 'Cette sauce ne vous intéresse plus !'}))
+                .catch(error => res.status(400).json({error}));
+            }
+            if (sauce.usersDisliked.includes(req.body.userId)){
+                ModelsSauce.updateOne(
+                    {_id: req.params.id}, 
+                    {$pull: {usersDisliked: req.body.userId},
+                    $inc: {dislikes: -1}}
+                )
+                .then(() => res.status(200).json({message: 'Cette sauce ne vous intéresse plus !'}))
+                .catch(error => res.status(400).json({error}));
+            }
+        })
+        .catch(error => res.status(400).json({error}));          
+    }
+
     if (req.body.like == -1) {
         ModelsSauce.updateOne(
             {_id: req.params.id}, 
-            {$push: {usersLiked: req.body.userId}},
-            {dislikes: -1}
+            {$push: {usersDisliked: req.body.userId},
+            $inc: {dislikes: +1}}
         )
         .then(() => res.status(200).json({message: 'Sauce disliké !'}))
         .catch(error => res.status(400).json({error}));  
-        console.log(ModelsSauce.usersLiked);
-        console.log(ModelsSauce.usersDisliked); 
+        console.log('Sauce disliké !'); 
     }
  console.log(req.body);
 };
